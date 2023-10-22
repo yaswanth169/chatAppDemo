d865d2feb2dfa0746d0299dc3c6bd257a586c751
yaswanth <yaswanth169choudary@gmail.com>
2023-10-22 20:44:05 +0530

    Initial commit

---
+++ src/test/java/com/chatapplication/chatAppDemo/ChatAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatapplication.chatAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
